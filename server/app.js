d78b519634bb8315a79ef610fe32bfd0faf4b711
Elizabeth Sanabria <143511550+beastpack-bright@users.noreply.github.com>
2024-12-14 16:06:00 -0500

    Update app.js

--- server/app.js
+++ server/app.js
@@ -12,9 +12,9 @@ const routes = require('./routes');
 const port = process.env.PORT || 3000;
 const app = express();
 const redisClient = createClient({
-  url: process.env.REDIS_URL || 'redis://localhost:6379',
+  url: process.env.REDIS_URL,
   socket: {
-    tls: process.env.NODE_ENV === 'production',
+    tls: true,
     rejectUnauthorized: false,
     connectTimeout: 20000,
     keepAlive: 5000,
